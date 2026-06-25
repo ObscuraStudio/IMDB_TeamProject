f45752cfc2be6ec0377f47c025e22d183bcd6a29
ObscuraStudio <maximilian.tofelde@gmail.com>
2026-06-25 16:41:58 +0200

    Revert "ignored .iml in frontend"
    This reverts commit ed688f496305bafce361a7612f07d8aea488fac2.

--- backend/src/main/java/org/example/backend/BackendApplication.java
+++ backend/src/main/java/org/example/backend/BackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieDatabase {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MovieDatabase.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
