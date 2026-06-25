2e0e9fb4f53fe56ecb611e3d93fe7f2b80be7ebb
Cathulhu <maximilian.tofelde@gmail.com>
2026-06-25 17:33:54 +0200

    restructured project

---
+++ backend/src/main/java/org/example/backend/MovieDatabase.java
@@ -0,0 +1,13 @@
+package org.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDatabase {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieDatabase.class, args);
+    }
+
+}
