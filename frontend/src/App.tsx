bceb193463bfc1fe10c796443f5068daa63e8ae6
emmanuel-santhosh <emmanuel_santhosh@yahoo.com>
2026-06-29 22:39:19 +0200

    - Installed axios, react-router-dom - Defined Homepage and NavBar components - Defined Routes section in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import {useState} from 'react'
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Homepage from "./components/Homepage.tsx";
+import NavBar from "./components/NavBar.tsx";
 
 function App() {
     
 
     return (
         <>
+            <NavBar></NavBar>
+            <Routes>
+                <Route path={"/"} element={<Homepage />}></Route>
+            </Routes>
         </>
     )
 }
