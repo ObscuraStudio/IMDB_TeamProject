b08dc48e8312d5ce0a5f034db2c9ff71e764db02
emmanuel-santhosh <emmanuel_santhosh@yahoo.com>
2026-07-06 09:53:49 +0200

    Defined interface for handling movie info from API

---
+++ frontend/src/types/OmdbMovie.ts
@@ -0,0 +1,30 @@
+export interface OmdbMovieResponse {
+    Title?: string;
+    Year?: string;
+    Rated?: string;
+    Released?: string;
+    Runtime?: string;
+    Genre?: string;
+    Director?: string;
+    Writer?: string;
+    Actors?: string;
+    Plot?: string;
+    Language?: string;
+    Country?: string;
+    Awards?: string;
+    Poster?: string;
+    Ratings?: Rating[];
+    Metascore?: string;
+    imdbRating?: string;
+    imdbVotes?: string;
+    imdbID?: string;
+    Type?: string;
+    BoxOffice?: string;
+    Response?: string;
+    Error?: string;
+}
+
+export interface Rating {
+    Source?: string;
+    Value?: string;
+}
