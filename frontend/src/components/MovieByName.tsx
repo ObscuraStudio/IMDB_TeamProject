2d251d6c2eb0bcd5c22f66b474ba5b94f16f49aa
emmanuel-santhosh <emmanuel_santhosh@yahoo.com>
2026-07-06 11:03:49 +0200

    Form elements for getMovieByName defined, dynamic functionality pending

--- frontend/src/components/MovieByName.tsx
+++ frontend/src/components/MovieByName.tsx
@@ -1,6 +1,25 @@
+import {useState} from "react";
+
 export default function MovieByName() {
+
+    const [movieName, setMovieName] = useState<string>("movie");
+
     return (
         <>
+            <form>
+                <label>Movie name (minimum 5 characters) </label>
+                <input onChange=
+                           {(e) =>
+                               setMovieName(e.target.value)
+                           }
+                    type="text"
+                    id="Movie name"
+                    name="Movie name"
+                    required
+                    minLength={4}
+                    placeholder={"Enter movie name"}/>
+                <button type={"submit"}></button>
+            </form>
         </>
     )
 }
