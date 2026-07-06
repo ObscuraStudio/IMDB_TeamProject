f03f730a3bc41d11d8b24f959709db2dfc89a576
emmanuel-santhosh <emmanuel_santhosh@yahoo.com>
2026-07-06 14:00:31 +0200

    - Base function for getMovieByName complete Pending: - Refactor to enable code reuse - Display more film details in an elegant way

--- frontend/src/components/MovieByName.tsx
+++ frontend/src/components/MovieByName.tsx
@@ -1,12 +1,25 @@
 import {useState} from "react";
+import axios from "axios";
+import type {OmdbMovieResponse} from "../types/OmdbMovie.ts";
 
 export default function MovieByName() {
+    const baseURI:string = "/api/movies";
+    const [movieName, setMovieName] = useState<string>("movie123");
+    const [movie, setMovie] = useState<OmdbMovieResponse>({Title: "", Year:""});
+    const [isMovieValid, setIsMovieValid] = useState<boolean>(true);
 
-    const [movieName, setMovieName] = useState<string>("movie");
+    function submitMovieName(event:React.SubmitEvent){
+        event.preventDefault();
+        const queryURI:string = baseURI + "/title/" + movieName ;
+        axios.get(queryURI)
+            .then((response) =>
+                (setMovie(response.data)))
+            .catch(()=>setIsMovieValid(false));
+    }
 
     return (
         <>
-            <form>
+            <form onSubmit={submitMovieName}>
                 <label>Movie name (minimum 5 characters) </label>
                 <input onChange=
                            {(e) =>
@@ -18,8 +31,14 @@ export default function MovieByName() {
                     required
                     minLength={4}
                     placeholder={"Enter movie name"}/>
-                <button type={"submit"}></button>
+                <button type={"submit"}> Submit </button>
             </form>
+            <div>
+                {isMovieValid ?
+                    <p>Movie details: {movie.Title} Year of release: {movie.Year}</p> :
+                    <p>Movie not found</p>
+                }
+            </div>
         </>
     )
 }
